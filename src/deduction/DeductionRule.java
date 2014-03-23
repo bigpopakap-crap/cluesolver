28b4255c12d1c00a1f73a8e53c8e9190bd633d4e
Kapil Easwar <bigpopakap@gmail.com>
2014-03-23 13:58:11 -0700

    started implementing deduction rules

--- src/deduction/DeductionRule.java
+++ src/deduction/DeductionRule.java
@@ -1,11 +1,12 @@
 package deduction;
 
+import game.GameInfo;
 import game.GameState;
 
 import java.util.List;
 
 public interface DeductionRule {
 	
-	public List<Conclusion> run(GameState state);
+	public List<Conclusion> run(GameState state, GameInfo info);
 
 }
--- src/deduction/DeductionRules.java
+++ src/deduction/DeductionRules.java
@@ -1,12 +1,45 @@
 package deduction;
 
+import game.GameInfo;
 import game.GameState;
 
 import java.util.Collections;
 import java.util.List;
 
+import player.Player;
+import card.Card;
+import checklist.ChecklistValue;
+
 public enum DeductionRules implements DeductionRule {
-	HORIZONTAL_UNIQUENESS,
+	/**
+	 * If a player is known to have a card, no other players have that card
+	 */
+	HORIZONTAL_UNIQUENESS {
+		@Override
+		public List<Conclusion> run(GameState state, GameInfo info) {
+			List<Conclusion> conclusions = Collections.emptyList();
+			
+			for (Player player : state.getPlayers()) {
+				for (Card card : state.getCardList().getAll()) {
+					run(state, info, conclusions, player, card);
+				}
+			}
+			
+			return conclusions;
+		}
+		
+		private void run(GameState state, GameInfo info, List<Conclusion> conclusions, Player player, Card card) {
+			if (info.getChecklist().getValue(player, card) == ChecklistValue.DEFINITELY_DOES_HAVE) {
+				//player has card, mark it as not had by any other player
+				for (Player player2 : state.getPlayers()) {
+					if (player.equals(player2)) continue;
+					
+					//TODO create conclusion to mark player2 as not having that card
+				}
+			}
+		}
+	},
+	
 	HORIZONTAL_UNIQUENESS_INVERSE,
 	
 	VERTICAL_EXACTNESS,
@@ -21,7 +54,7 @@ public enum DeductionRules implements DeductionRule {
 	REFUTATION_DEDUCED;
 
 	@Override
-	public List<Conclusion> run(GameState state) {
+	public List<Conclusion> run(GameState state, GameInfo info) {
 		// default: don't do anything
 		return Collections.emptyList();
 	}
