bc1f537829f4b5c4ed853b1aad766ef16b5695ab
Kapil Easwar <bigpopakap@gmail.com>
2014-03-23 21:01:37 -0700

    added deduction runner

--- src/logic/DeductionRunner.java
+++ src/logic/DeductionRunner.java
@@ -3,24 +3,43 @@ package logic;
 import game.GameInfo;
 import game.GameState;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import logic.conclusion.Conclusion;
 import logic.deduction.Deduction;
+import logic.deduction.HorizontalUniquenessDeduction;
+import logic.deduction.HorizontalUniquenessInverseDeduction;
 
 public class DeductionRunner implements Deduction {
 	
 	//TODO use a builder so that you can easily choose not to use all deductions
 	
 	private static final List<Deduction> DEDUCTIONS = Arrays.asList(
-		
+		new HorizontalUniquenessDeduction(),
+		new HorizontalUniquenessInverseDeduction()
 	);
 
 	@Override
 	public List<Conclusion> run(GameState state, GameInfo info) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Conclusion> conclusions = new ArrayList<>();
+		
+		boolean conclusionsAdded;
+		do {
+			conclusionsAdded = false;
+			for (Deduction deduction : DEDUCTIONS) {
+				List<Conclusion> deductionConclusions = deduction.run(state, info);
+				
+				if (!deductionConclusions.isEmpty()) {
+					conclusionsAdded = true;
+					conclusions.addAll(deductionConclusions);
+				}
+			}
+		}
+		while (conclusionsAdded);
+		
+		return conclusions;
 	}
 
 }
